c94cae2cb485b9849721f3cb3fe45647c64b1ad0
jarryyou <1021845916@qq.com>
2025-01-22 17:09:09 +0800

    feat: 1.重新设计文件目录结构;

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,18 @@
 <template>
-
+  <Header></Header>
+  <PicWraper></PicWraper>
+  <GenContents></GenContents>
+  <Properties></Properties>
+  <Creators></Creators>
+  <Footer></Footer>
 </template>
 <script lang="ts" setup>
-
+import Header from './component/header/index.vue'
+import PicWraper from './component/picWrapper/index.vue'
+import GenContents from './component/genContents/index.vue'
+import Properties from './component/properties/index.vue'
+import Creators from './component/creators/index.vue'
+import Footer from './component/footer/index.vue'
 </script>
 <style scoped>
 
