d1317f0fd7a472273bd75a116c1b67956fb8c4df
jarryyou <1021845916@qq.com>
2025-01-22 17:07:42 +0800

    feat: 1.重新设计文件目录结构;

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,7 @@
 <template>
-  <Header></Header>
-  <PicWraper></PicWraper>
-  <GenContents></GenContents>
-  <Properties></Properties>
-  <Creators></Creators>
-  <Footer></Footer>
+  <router-view></router-view>
 </template>
 <script lang="ts" setup>
-import Header from './views/header/index.vue'
-import PicWraper from './views/picWrapper/index.vue'
-import GenContents from './views/genContents/index.vue'
-import Properties from './views/properties/index.vue'
-import Creators from './views/creators/index.vue'
-import Footer from './views/footer/index.vue'
 </script>
 <style lang="scss" scoped>
 
