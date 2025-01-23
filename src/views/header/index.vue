efc58da5cd061c8bb5529d3dd7d6fa904608a835
jarryyou <1021845916@qq.com>
2025-01-23 16:18:14 +0800

    feat: 1.header右侧按钮跳转事件及右侧按钮高亮颜色调整;

--- src/views/header/index.vue
+++ src/views/header/index.vue
@@ -5,7 +5,12 @@
       <img class="another-me" src="@/assets/images/Another_Me.png" width="130" height="20" alt="Another_Me">
     </div>
     <ul class="links">
-      <li class="link-item" v-for="(o, i) in linkLists" :key="i" @click="() => go(o)">
+      <li
+          :class="['link-item', currentLink === o.path ? 'active-link' : '']"
+          v-for="(o, i) in linkLists"
+          :key="i"
+          @click="() => go(o)"
+      >
         {{ o.name }}
       </li>
     </ul>
@@ -13,15 +18,22 @@
 </template>
 <script lang="ts" setup>
 import { computed } from "vue";
-import { useRouter } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
+import type {LinkItem} from "@/views/header/header.ts";
 
-const activeLink = computed(() => {
-  return linkLists.find(s => s.value === useRouter().currentRoute.value.name)
+const router = useRouter()
+const route = useRoute()
+const currentLink = computed(() => {
+  return route.path
 })
-const linkLists = [
+const linkLists: LinkItem[] = [
   { name: '平台介绍', path: '/home', value: 'home' },
   { name: '下载APP', path: '/download', value: 'download' }
 ]
+
+const go = (o: LinkItem) => {
+  router.push(o.path)
+}
 </script>
 <style lang="scss" scoped>
 .header {
@@ -51,12 +63,18 @@ const linkLists = [
     .link-item {
       padding: 2px 25px;
       height: 36px;
-      background-color: black;
+      background-color: #ffffff;
       border-radius: 20px;
       margin: 0 20px;
-      color: #ffffff;
+      color: black;
       display: flex;
       align-items: center;
+      cursor: pointer;
+    }
+    
+    .active-link {
+      background-color: black;
+      color: #ffffff;
     }
   }
 }
